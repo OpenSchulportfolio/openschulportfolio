c2371767e2ee795cf667a7da24966c2dc14ed23f
Andreas Gohr <andi@splitbrain.org>
2018-12-06 15:25:21 +0100

    we need to extract the icalevents plugin from zip
    it is a build artifact not available directly from the github branch.
    
    The aproach to zip files is a bit naive but works for now.

--- lib.sh
+++ lib.sh
@@ -1,17 +1,27 @@
+# show a message and exit with code 1
+#
+# @param {string} message
 die() {
     echo "$1"
     exit 1
 }
 
+# extract the given URL to the given directory
+#
+# We assume that zip files have no top directory while tgz files have (which we strip)
+# This is a very naive approach but works for our use case currently
+#
+# @param {string} url to download
+# @param {string} destination path to unpack into
 fetchAndInstall() {
     SRC=$1
     DST=$2
 
     if [[ -z "$DST" ]]; then
-        DL='dokuwiki.tgz'
+        DL='dokuwiki.archive'
     else
         DL=`basename $DST`;
-        DL="$DL.tgz"
+        DL="$DL.archive"
     fi
 
     if [[ ! -s "$CACHE/$DL" ]]; then
@@ -19,9 +29,19 @@ fetchAndInstall() {
     fi
 
     mkdir -p "$OUT/$DST"
-    tar -xzvf "$CACHE/$DL" --strip-components 1 -C "$OUT/$DST" || die 'Failed to extract archive'
+
+
+    if file -b "$CACHE/$DL" | grep 'Zip archive'; then
+        unzip "$CACHE/$DL" -d "$OUT/$DST/"  || die 'Failed to extract ZIP archive'
+    else
+        tar -xzvf "$CACHE/$DL" --strip-components 1 -C "$OUT/$DST" || die 'Failed to extract TGZ archive'
+    fi
 }
 
+# download and install the master branch of the given repo
+#
+# @param {string} repo in the form user/reponame
+# @param {string} destination path to unpack into
 githubInstall() {
     REPO=$1
     DST=$2
