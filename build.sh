348a4a1f5649ff89d518e1853fd3bc29a65bbc8c
Frank Schiebel <frank@linuxmuster.net>
2020-11-23 15:12:14 +0100

    Release 20.11-snoutlout.5

--- build.sh
+++ build.sh
@@ -47,6 +47,7 @@ githubInstall "dokufreaks/plugin-blog"                      "lib/plugins/blog"
 githubInstall "dokufreaks/plugin-pagelist"                  "lib/plugins/pagelist"
 githubInstall "dokufreaks/plugin-include"                   "lib/plugins/include"
 githubInstall "dokufreaks/plugin-cloud"                     "lib/plugins/cloud"
+githubInstall "splitbrain/dokuwiki-plugin-smtp"             "lib/plugins/smtp"
 
 githubInstall "splitbrain/dokuwiki-plugin-talkpage"         "lib/plugins/talkpage"
 githubInstall "splitbrain/dokuwiki-plugin-dw2pdf"           "lib/plugins/dw2pdf"
@@ -94,8 +95,8 @@ for patch in $(find patches -name '*.patch'); do
     filetopatch=${patch%*.patch}
     filetopatch=${filetopatch#patches/}
     filetopatch=${OUT}/$filetopatch
+    echo " Running: patch  -u $filetopatch  -i $patch"
     patch  -u $filetopatch  -i $patch
-
 done
 
 echo 
@@ -111,9 +112,11 @@ echo -n " Creating install package (zip)..."
 zip -q -r "${OUT}-${version}-install.zip" "$OUT"
 echo " done."
 
-
+cp ${OUT}/conf/dokuwiki.php ./dwphp.tmp
 rm -rf ${OUT}/conf
 rm -rf ${OUT}/data
+mkdir ${OUT}/conf
+mv ./dwphp.tmp ${OUT}/conf/dokuwiki.php
 echo -n " Creating update package (tar)..."
 tar -czf "${OUT}-${version}-update.tgz" "$OUT"
 echo " done."
